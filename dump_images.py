6a2c229d2d089dcb76b0fc34c27148c182167e15
Suraj <surajsemailid@gmail.com>
2019-09-25 17:19:10 +0200

    edit

--- dump_images.py
+++ dump_images.py
@@ -41,7 +41,7 @@ unnormalize = NormalizeInverse(mean = [0.485, 0.456, 0.406],
                            std = [0.229, 0.224, 0.225])
 
 
-model = vgg11(pretrained=True)
+model = vgg16_bn(pretrained=True)
 
 # Initialize FullGrad object
 fullgrad = FullGrad(model)
